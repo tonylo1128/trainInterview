f56e6d2b96085019545672d5d391f0f5f593c676
tony.lo <tony.lo@arch-partnership.com>
2022-03-16 20:40:55 +0800

    refactor: helper function refactor

--- src/helper.js
+++ src/helper.js
@@ -1,37 +1,61 @@
+// export default function filteration(filterationType, train, setSortedTrain) {
+//   let temp = {};
+
+//   switch (filterationType) {
+//     case "LineCode":
+//       train.map((item, index) => {
+//         if (!temp[item.LineCode]) {
+//           temp[item.LineCode] = [];
+//         }
+//         temp[item.LineCode].push(item);
+//       });
+//       console.log(temp);
+//       setSortedTrain(temp);
+//       break;
+
+//     case "ServiceType":
+//       train.map((item, index) => {
+//         if (!temp[item.ServiceType]) {
+//           temp[item.ServiceType] = [];
+//         }
+//         temp[item.ServiceType].push(item);
+//       });
+//       console.log(temp);
+//       setSortedTrain(temp);
+//       break;
+
+//     case "CarCount":
+//       train.map((item, index) => {
+//         if (!temp[item.CarCount]) {
+//           temp[item.CarCount] = [];
+//         }
+//         temp[item.CarCount].push(item);
+//       });
+//       console.log(temp);
+//       setSortedTrain(temp);
+//       break;
+
+//     case "all":
+//     default:
+//       temp["all"] = [...train];
+//       setSortedTrain(temp);
+//   }
+// }
+
 export default function filteration(filterationType, train, setSortedTrain) {
   let temp = {};
 
   switch (filterationType) {
     case "LineCode":
-      train.map((item, index) => {
-        if (!temp[item.LineCode]) {
-          temp[item.LineCode] = [];
-        }
-        temp[item.LineCode].push(item);
-      });
-      console.log(temp);
-      setSortedTrain(temp);
-      break;
-
     case "ServiceType":
-      train.map((item, index) => {
-        if (!temp[item.ServiceType]) {
-          temp[item.ServiceType] = [];
-        }
-        temp[item.ServiceType].push(item);
-      });
-      console.log(temp);
-      setSortedTrain(temp);
-      break;
-
     case "CarCount":
-      train.map((item, index) => {
-        if (!temp[item.CarCount]) {
-          temp[item.CarCount] = [];
+      train.map((item) => {
+        const bufferType = item[filterationType];
+        if (!temp[bufferType]) {
+          temp[bufferType] = [];
         }
-        temp[item.CarCount].push(item);
+        temp[bufferType].push(item);
       });
-      console.log(temp);
       setSortedTrain(temp);
       break;
 
